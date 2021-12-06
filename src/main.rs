163b80c1466f746f153eacb93dedb56c05c473fd
Jason Lee <huacnlee@gmail.com>
2021-12-06 22:05:14 +0800

    Update example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use rust_i18n::t;
+#[macro_use]
+extern crate rust_i18n;
+
+// Init translations for current crate.
+i18n!("locales");
 
 fn main() {
-    rust_i18n::set_locale("fr-FR");
     println!("{}, Longbridge!", t!("hello"));
+
+    //
+    println!("{}", example_base::hello("Longbridge"));
+
+    rust_i18n::set_locale("fr");
+    println!("{}, Longbridge!", t!("hello"));
+    println!("{}", example_base::hello("Longbridge"));
 }
